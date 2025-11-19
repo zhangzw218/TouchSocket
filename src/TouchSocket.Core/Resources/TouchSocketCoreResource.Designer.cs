76a31147ed00f1a9766f25dee8eaaa7c036714b0
张志炜 <745787360@qq.com>
2025-11-19 15:03:04 +0800

    TouchSocketV4版本

--- src/TouchSocket.Core/Resources/TouchSocketCoreResource.Designer.cs
+++ src/TouchSocket.Core/Resources/TouchSocketCoreResource.Designer.cs
@@ -39,7 +39,7 @@ namespace TouchSocket.Resources {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TouchSocket.Core.Resources.TouchSocketCoreResource", typeof(TouchSocketCoreResource).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TouchSocketV4.Core.Resources.TouchSocketCoreResource", typeof(TouchSocketCoreResource).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
