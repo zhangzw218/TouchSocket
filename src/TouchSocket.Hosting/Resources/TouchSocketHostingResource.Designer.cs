9d436d1961e80b0678f77f5bdffc4b2e90e352a8
张志炜 <745787360@qq.com>
2025-11-26 22:10:53 +0800

    项目重命名V4

--- src/TouchSocket.Hosting/Resources/TouchSocketHostingResource.Designer.cs
+++ src/TouchSocket.Hosting/Resources/TouchSocketHostingResource.Designer.cs
@@ -19,7 +19,7 @@ namespace TouchSocket.Resources {
     // 类通过类似于 ResGen 或 Visual Studio 的工具自动生成的。
     // 若要添加或移除成员，请编辑 .ResX 文件，然后重新运行 ResGen
     // (以 /str 作为命令选项)，或重新生成 VS 项目。
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "18.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class TouchSocketHostingResource {
@@ -39,7 +39,7 @@ namespace TouchSocket.Resources {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TouchSocket.Hosting.Resources.TouchSocketHostingResource", typeof(TouchSocketHostingResource).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TouchSocketV4.Hosting.Resources.TouchSocketHostingResource", typeof(TouchSocketHostingResource).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
